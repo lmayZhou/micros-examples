2ac8eec097bb7a89d1375c10e9b5ce8477d01c89
lmay <lmay@lmaye.com>
2020-12-22 12:15:27 +0800

    -- feat: oauth.

---
+++ ms-services/ms-service-oauth/src/main/java/com/lmaye/ms/service/oauth/config/MsServiceOauthConfig.java
@@ -0,0 +1,14 @@
+package com.lmaye.ms.service.oauth.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * -- Ms Service Oauth Config
+ *
+ * @author lmay.Zhou
+ * @date 2020/12/21 17:07
+ * @email lmay@lmaye.com
+ */
+@Configurable
+public class MsServiceOauthConfig {
+}
