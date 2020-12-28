33cae69d768fe66c793fbaf5c1e4a4385c7fcfea
lmay <lmay@lmaye.com>
2020-12-28 23:25:59 +0800

    -- feat: gateway.

--- ms-gateway/ms-gateway-web/src/main/java/com/lmaye/ms/gateway/web/WebGatewayApplication.java
+++ ms-gateway/ms-gateway-web/src/main/java/com/lmaye/ms/gateway/web/WebGatewayApplication.java
@@ -1,10 +1,10 @@
-package com.lmaye.ms.gateway;
+package com.lmaye.ms.gateway.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * -- Gateway Application
+ * -- Web Gateway Application
  *
  * @author lmay.Zhou
  * @date 2020/12/28 22:49 星期一
@@ -13,8 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since JDK1.8
  */
 @SpringBootApplication
-public class MsGatewayApplication {
+public class WebGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MsGatewayApplication.class, args);
+        SpringApplication.run(WebGatewayApplication.class, args);
     }
 }
--- ms-gateway/ms-gateway-web/src/main/java/com/lmaye/ms/gateway/web/filter/AuthorizeFilter.java
+++ ms-gateway/ms-gateway-web/src/main/java/com/lmaye/ms/gateway/web/filter/AuthorizeFilter.java
@@ -1,4 +1,4 @@
-package com.lmaye.ms.gateway.filter;
+package com.lmaye.ms.gateway.web.filter;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
