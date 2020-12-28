b3a51bb53be107cee745a74784da602e9473433d
lmay <lmay@lmaye.com>
2020-12-28 23:41:11 +0800

    -- style: 配置文件.

--- ms-gateway/ms-gateway-web/src/main/java/com/lmaye/ms/gateway/web/WebGatewayApplication.java
+++ ms-gateway/ms-gateway-web/src/main/java/com/lmaye/ms/gateway/web/WebGatewayApplication.java
@@ -2,6 +2,7 @@ package com.lmaye.ms.gateway.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * -- Web Gateway Application
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since JDK1.8
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebGatewayApplication.class, args);
