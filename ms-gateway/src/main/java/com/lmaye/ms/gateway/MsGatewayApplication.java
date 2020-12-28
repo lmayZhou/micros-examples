5ff037952cbeb66a58e0550afe1edd145fb5c313
lmay <lmay@lmaye.com>
2020-12-28 22:49:56 +0800

    -- feat: 网关.

--- ms-gateway/src/main/java/com/lmaye/ms/gateway/MsGatewayApplication.java
+++ ms-gateway/src/main/java/com/lmaye/ms/gateway/MsGatewayApplication.java
@@ -3,11 +3,18 @@ package com.lmaye.ms.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * -- Gateway Application
+ *
+ * @author lmay.Zhou
+ * @date 2020/12/28 22:49 星期一
+ * @qq 379839355
+ * @email lmay@lmaye.com
+ * @since JDK1.8
+ */
 @SpringBootApplication
 public class MsGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsGatewayApplication.class, args);
     }
-
 }
--- ms-gateway/src/main/java/com/lmaye/ms/gateway/filter/AuthorizeFilter.java
+++ ms-gateway/src/main/java/com/lmaye/ms/gateway/filter/AuthorizeFilter.java
@@ -11,6 +11,8 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 /**
  * -- 全局过滤器 :用于鉴权(获取令牌 解析 判断)
  *
@@ -19,45 +21,61 @@ import reactor.core.publisher.Mono;
  * @email lmay@lmaye.com
  */
 public class AuthorizeFilter implements GlobalFilter, Ordered {
+    /**
+     * Authorize Token
+     */
     private static final String AUTHORIZE_TOKEN = "Authorization";
 
+    /**
+     * No Intercept URL
+     */
     private static final String NO_INTERCEPT_URL = "/api/user/login,/api/user/add";
 
+    /**
+     * OAuth Login
+     */
     private static final String OAUTH_LOGIN = "http://localhost:9001/oauth/login";
 
+    /**
+     * 拦截
+     *
+     * @param exchange ServerWebExchange
+     * @param chain    GatewayFilterChain
+     * @return Mono<Void>
+     */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        //1.获取请求对象
+        // 1.获取请求对象
         ServerHttpRequest request = exchange.getRequest();
-        //2.获取响应对象
+        // 2.获取响应对象
         ServerHttpResponse response = exchange.getResponse();
-        //3.判断 是否为登录的URL 如果是 放行
+        // 3.判断 是否为登录的URL 如果是 放行
         if (hasAutoresize(request.getURI().toString())) {
             return chain.filter(exchange);
         }
-        //4.判断 是否为登录的URL 如果不是      权限校验
-        //4.1 从头header中获取令牌数据
+        // .判断 是否为登录的URL 如果不是      权限校验
+        // 4.1 从头header中获取令牌数据
         String token = request.getHeaders().getFirst(AUTHORIZE_TOKEN);
         if (StringUtils.isEmpty(token)) {
-            //4.2 从cookie中中获取令牌数据
+            // 4.2 从cookie中中获取令牌数据
             HttpCookie first = request.getCookies().getFirst(AUTHORIZE_TOKEN);
-            if (first != null) {
+            if (!Objects.isNull(first)) {
                 // 就是令牌的数据
                 token = first.getValue();
             }
         }
         if (StringUtils.isEmpty(token)) {
-            //4.3 从请求参数中获取令牌数据
+            // 4.3 从请求参数中获取令牌数据
             token = request.getQueryParams().getFirst(AUTHORIZE_TOKEN);
         }
         if (StringUtils.isEmpty(token)) {
-            //4.4. 如果没有数据没有登录,要重定向到登录到页面(303 302)
+            // 4.4. 如果没有数据没有登录,要重定向到登录到页面(303 302)
             response.setStatusCode(HttpStatus.SEE_OTHER);
             //location 指定的就是路径
             response.getHeaders().set("Location", OAUTH_LOGIN + "?From=" + request.getURI().toString());
             return response.setComplete();
         }
-        //5 解析令牌数据 ( 判断解析是否正确,正确 就放行 ,否则 结束)
+        // 5 解析令牌数据 ( 判断解析是否正确,正确 就放行 ,否则 结束)
         /*try {
             Claims claims = JwtUtil.parseJWT(token);
         } catch (Exception e) {
@@ -66,7 +84,7 @@ public class AuthorizeFilter implements GlobalFilter, Ordered {
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
             return response.setComplete();
         }*/
-        //添加头信息 传递给 各个微服务()
+        // 添加头信息 传递给 各个微服务()
         request.mutate().header(AUTHORIZE_TOKEN, "Bearer " + token);
         return chain.filter(exchange);
     }
@@ -74,6 +92,7 @@ public class AuthorizeFilter implements GlobalFilter, Ordered {
     /**
      * 用来判断 如果 当前的请求 在 放行的请求中存在,(不需要拦截 :true,否则需要拦截:false)
      *
+     * @param uri URI
      * @return boolean
      */
     public static boolean hasAutoresize(String uri) {
@@ -84,7 +103,7 @@ public class AuthorizeFilter implements GlobalFilter, Ordered {
                 return true;
             }
         }
-        //要拦截
+        // 要拦截
         return false;
     }
 
