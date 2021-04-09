8cddf68f7e67b44ecd0234c984d4d1c1a4aed0ea
lmay <lmay@lmaye.com>
2021-04-09 13:42:21 +0800

    feat: TODO canal.

---
+++ ms-canal/src/main/java/com/lmaye/ms/canal/MsCanalApplication.java
@@ -0,0 +1,13 @@
+package com.lmaye.ms.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCanalApplication.class, args);
+    }
+
+}
---
+++ ms-canal/src/test/java/com/lmaye/ms/canal/MsCanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmaye.ms.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
